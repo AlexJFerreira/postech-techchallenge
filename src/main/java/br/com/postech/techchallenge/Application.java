c97e13ed9af45a147dc18a5b8a5372f861670332
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-03-19 13:53:59 -0300

    adding step

--- src/main/java/br/com/postech/techchallenge/Application.java
+++ src/main/java/br/com/postech/techchallenge/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		log.info("logando com versão nova 3");
+		System.out.println("Starting application");
 		SpringApplication.run(Application.class, args);
 	}
 
